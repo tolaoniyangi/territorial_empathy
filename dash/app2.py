69ef11f6781c4f7deeca07437a7c83407d28097e
tolaoniyangi <tolaoniyangi.com>
2020-06-24 17:54:55 -0400

    first pass

---
+++ dash/app2.py
@@ -0,0 +1,32 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/nychealth/coronavirus-data/3f21405a00bf7c93db5a5209b44886882feb30a6/by-boro.csv')
+
+
+def generate_table(dataframe, max_rows=10):
+    return html.Table([
+        html.Thead(
+            html.Tr([html.Th(col) for col in dataframe.columns])
+        ),
+        html.Tbody([
+            html.Tr([
+                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+            ]) for i in range(min(len(dataframe), max_rows))
+        ])
+    ])
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H4(children='US Agriculture Exports (2011)'),
+    generate_table(df)
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
